dddda28c8136bfe276b58464fa0eee6500594eb6
x04000 <78761999+x04000@users.noreply.github.com>
2022-02-07 20:25:27 +0100

    Add files via upload

---
+++ pr1smakiller/Pr1smaUpdater.py
@@ -0,0 +1,17 @@
+import time
+import os
+print("[Pr1smaKiller Updater]")
+print("by x04000")
+print("")
+print("Updating Pr1smaKiller...")
+time.sleep(1)
+try:
+	os.system("cd ..")
+	os.system("wget https://raw.githubusercontent.com/x04000/Pr1smaKiller/main/Pr1smaKiller.py")
+except KeyboardInterrupt:
+	print("[X] KeyBoardInterrupt")
+except:
+	print("[X] A error ocurred during the updating")
+time.sleep(1)
+print("If you experiment problems with Pr1smaKiller, please reinstall them.")
+print("Operation finished!")
